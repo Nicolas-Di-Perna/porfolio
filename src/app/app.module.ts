195e3ffa658e47c842e96a5bc9b2b0340a2826e3
Diper94 <nicolasdiperna@gmail.com>
2022-05-16 00:45:05 -0300

    logos redes ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LogoAPComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
