32dc6c30e1d05fa464c5beb1e3f879c424a00a9e
Diper94 <nicolasdiperna@gmail.com>
2022-05-16 16:50:25 -0300

    banner listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { LoginComponent } from './components/login/login.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { RedesComponent } from './components/redes/redes.component';
     HeaderComponent,
     FooterComponent,
     LogoAPComponent,
-    RedesComponent
+    RedesComponent,
+    LoginComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
