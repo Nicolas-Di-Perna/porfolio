10b9a8a92a27bfc243421b52a69ad85673890d21
Diper94 <nicolasdiperna@gmail.com>
2022-06-28 18:56:21 -0300

    28/06

--- Back/src/main/java/com/porfolio/nicolas/Controller/PersonaController.java
+++ Back/src/main/java/com/porfolio/nicolas/Controller/PersonaController.java
@@ -54,7 +54,7 @@ public class PersonaController {
    
      @GetMapping("/personas/traer/perfil")
     public Persona findPersona(){
-        return ipersonaService.findPersona((long)3);
+        return ipersonaService.findPersona((long)1);
     }
 
 }
